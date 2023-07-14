1e3959613db16305e437c0e029b5504b3434ee3b
avacosta <avacosta@ossesanjuan.com.ar>
2023-07-14 11:40:37 -0300

    JWT Generator

---
+++ BuberDinner.Infraestructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,56 @@
+using System.Security.Claims;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Cryptography;
+using BuberDinner.Application.Common.Interfaces.Services;
+using Microsoft.Extensions.Options;
+
+namespace BuberDinner.Infraestructure.Authentication;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly JwtSettings _jwtSettings;
+
+    public JwtTokenGenerator(JwtSettings jwtSettings)
+    {
+        _jwtSettings = jwtSettings;
+    }
+
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider, IOptions<JwtSettings> jwtOptions)
+    {
+        _dateTimeProvider = dateTimeProvider;
+        _jwtSettings = jwtOptions.Value;
+    }
+
+    public string GenerateToken(Guid userId, string firstName, string lastName)
+    {
+        
+        
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
+            SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())                 
+        };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: _jwtSettings.Issuer,
+            audience: _jwtSettings.Audience,
+            expires: _dateTimeProvider.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
+            claims:claims,
+            signingCredentials:signingCredentials
+            );
+        
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+}
